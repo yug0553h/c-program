71aa0258f3e3f3ae9b89f67219e9e4fb37ea1191
yug0553h <yugvasava240@gmail.com>
2025-08-28 22:58:01 +0530

    Add files via upload

---
+++ Subtract two number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+int a,b;
+printf("enter the value of a:");
+scanf("%d",&a);
+printf("enter the value of b:");
+scanf("%d",&b);
+int substraction=a-b;
+printf("substraction of a and b is:%d",substraction);
+
+}
