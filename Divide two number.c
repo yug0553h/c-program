a5a87ed58f751b21ea6fc848e386a78f7e38c3d7
yug0553h <yugvasava240@gmail.com>
2025-08-28 22:58:52 +0530

    Add files via upload

---
+++ Divide two number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+float a,b;
+printf("enter the value of a:");
+scanf("%f",&a);
+printf("enter the value of b:");
+scanf("%f",&b);
+float division=a/b;
+printf("division of a and b is:%f",division);
+
+}
