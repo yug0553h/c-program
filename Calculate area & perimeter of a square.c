c6c8b4455bf6e6146017de8b73d1b900a0e3bcb5
yug0553h <yugvasava240@gmail.com>
2025-08-28 23:04:58 +0530

    Add files via upload

---
+++ Calculate area & perimeter of a square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    float L;
+    printf("Enter the Length of square:");
+    scanf("%f",&L);
+     printf(" Area of square is:%.2f\n",L*L);
+
+     printf("Parametar of square is:%.2f\n",4*L);
+
+
+return 0;
+}
+
