29ce4acb25c4f80a7f42a6359857d830162bf712
yug0553h <yugvasava240@gmail.com>
2025-08-28 22:59:09 +0530

    Add files via upload

---
+++ Add,multipy,subtract and divide two number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    float a,b;
+    printf("Enter the a:");
+    scanf("%f",&a);
+    printf("Enter the b:");
+    scanf("%f",&b);
+    printf("sum is:%.2f\n",a+b);
+    printf("substraction is:%.2f\n",a-b);
+    printf("multiplication is:%f\n",a*b);
+    printf("division is:%f\n",a/b);
+return 0;
+}
