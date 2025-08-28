0d53fa9bd5296f1520eb65cb4f3b1182168af63e
yug0553h <yugvasava240@gmail.com>
2025-08-28 22:59:42 +0530

    Add files via upload

---
+++ Convert hour into minutes.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    float Hours;
+    printf("enter time in Hour:");
+    scanf("%f",&Hours);
+    printf("value of Time in Minutes:%.2f",Hours*60);
+
+return 0;
+
+}
