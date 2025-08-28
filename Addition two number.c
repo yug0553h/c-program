9f67a197d9c0f8088fc3f90240587c76f2f69354
yug0553h <yugvasava240@gmail.com>
2025-08-28 22:57:36 +0530

    Add files via upload

---
+++ Addition two number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+int a,b;
+printf("enter the value of a:");
+scanf("%d",&a);
+printf("enter the value of b:");
+scanf("%d",&b);
+int sum=a+b;
+printf("sum of a and b is:%d",sum);
+
+}
