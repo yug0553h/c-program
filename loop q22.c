27ffb3c84e1d75692e42e715e53d1426c878a608
yug0553h <yugvasava240@gmail.com>
2025-11-15 17:27:12 +0530

    Create loop q22.c

---
+++ loop q22.c
@@ -0,0 +1,17 @@
+
+//22.	Count how many digits are there in a given no. e.g. 351-3.y
+#include <stdio.h>
+int main()   {
+	int a=0,b=0;
+	printf("write a number:");
+	scanf("%d",&a);
+	if(a==0)  {  printf("%d",1);
+	}
+	else {
+	
+	while(a!=0) {a/=10; b++;
+	}}
+	printf("%d",b);
+	
+	
+}
