16883f50bae00e6f596127e45d327fdb354f2bd0
yug0553h <yugvasava240@gmail.com>
2025-11-15 17:27:56 +0530

    Create loop q24.c

---
+++ loop q24.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+int main()   {
+	int a,i;
+	printf("write a number:");
+	scanf("%d",&a);
+	while (a!=0) {
+	
+	
+	i=a%10;
+	a=a/10;
+
+
+
+	printf(" %d \n",i);}
+	return 0;
+	
+	
+	
+	
+	
+}
