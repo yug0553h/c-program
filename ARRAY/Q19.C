9bffe6b0136f79835fc0d22edc2d1d5d6be4af6b
yug0553h <yugvasava240@gmail.com>
2025-11-15 16:54:25 +0530

    Implement array search functionality
    Add a program to search for a value in an array.

---
+++ ARRAY/Q19.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+	int a,flag=0;
+	int arr[5]={5,45,23,56,12};
+	printf("search in arr:");
+	scanf("%d",&a);
+	
+	for(int i=0;i<=4;i++) {
+		
+		
+		if(a==arr[i])   {
+		
+			printf("value is available in arr array.");
+			break;
+			
+			
+	}
+			
+			
+	}
+		
+
+	
+	 printf("not available");
+	
+	
+	
+}
