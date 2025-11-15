aa30270ed1d07a9e309f4921749066d03a8ea2ea
yug0553h <yugvasava240@gmail.com>
2025-11-15 17:56:16 +0530

    Create USING IF CONDITION Q1.C

---
+++ USING IF CONDITION Q1.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()    {
+
+
+    int a,b;
+    printf ("value for a:");
+    scanf("%d",&a);
+    printf("value for b:");
+    scanf("%d",&b);
+
+    if (a>b)
+    printf("the largest value is:%d",a);
+
+    else
+        printf("the lowest  value is:%d",a);
+
+
+    return 0;
