ba540c7eabceb312cc70287c24e0bfaee2f49ada
yug0553h <yugvasava240@gmail.com>
2025-11-15 16:38:28 +0530

    Create Q2.C

---
+++ ARRAY/Q2.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()  {
+	
+	int arr[10];
+	printf("write array elements:");
+	for(int i=0;i<=9;i++) {
+		scanf("%d",&arr[i]);
+	}
+	
+		
+		printf("4th%d\n 7th%d\n 9th%d", arr[3],arr[6],arr[8]);
+	}
+	
