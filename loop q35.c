c51a13bc44d22b5caeca79f269609736bc8ea867
yug0553h <yugvasava240@gmail.com>
2025-11-15 17:32:25 +0530

    Create loop q35.c

---
+++ loop q35.c
@@ -0,0 +1,60 @@
+
+
+//A
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 2; i++) {
+        for (int j = 1; j <= 3; j++) {
+            printf("%d %d\n", i, j);
+        }
+    }
+    return 0;
+}
+
+//B
+
+
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d %d\n", i, j);
+        }
+    }
+    return 0;
+}
+
+
+//C
+
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 2; i++) {
+        for (int j = 1; j <= 2; j++) {
+            for (int k = 1; k <= 2; k++) {
+                printf("%d %d %d\n", i, j, k);
+            }
+        }
+    }
+    return 0;
+}
+
+
+//D
+
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 5; i >= 3; i--) {
+        for (int j = 1; j <= 2; j++) {
+            printf("%d %d\n", i, j);
+        }
+    }
+    return 0;
+}
