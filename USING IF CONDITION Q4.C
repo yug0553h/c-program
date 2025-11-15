848c0321be8fa27934f54b9fc49ed41899ddc714
yug0553h <yugvasava240@gmail.com>
2025-11-15 17:58:46 +0530

    Create USING IF CONDITION Q4.C

---
+++ USING IF CONDITION Q4.C
@@ -0,0 +1,13 @@
+// a number is divisibe by 7 or not.
+#include <stdio.h>
+int main() {
+	int a;
+	printf("write a digit:");
+	scanf("%d",&a);
+	if (a%7==0)
+	printf("this number is divisible by 7");
+	else 
+	printf("this number is not divisible by 7");	
+	
+	
+}
